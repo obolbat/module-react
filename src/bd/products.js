fb6cc7c007c1feb5821e0e3bc78b371bedaf348a
obolbat <it-for-bi@yandex.ru>
2023-04-24 21:20:59 +0600

    Описание карточек товара реализовано в масстве отдельным файлом, данные выведены при помощи map

---
+++ src/bd/products.js
@@ -0,0 +1,83 @@
+import img1 from "../assets/1.png";
+import img2 from "../assets/2.png";
+import img3 from "../assets/3.png";
+
+const products = [
+    {
+        id: 1,
+        productPreview: img1,
+        productTitle: "Устрицы по рокфеллеровски",
+        description: "Значимость этих проблем настолько очевидна, что укрепление и развитие структуры",
+        price: 2700,
+        weight: 500
+    },
+    {
+        id: 2,
+        productPreview: img2,
+        productTitle: "Свиные ребрышки на гриле с зеленью",
+        description: "Не следует, однако забывать, что реализация намеченных плановых",
+        price: 1600,
+        weight: 750
+    },
+    {
+        id: 3,
+        productPreview: img3,
+        productTitle: "Креветки по-королевски в лимонном соке",
+        description: "Значимость этих проблем настолько очевидна, что укрепление и развитие структуры обеспечивает широкому кругу",
+        price: 1820,
+        measure: "шт.",
+        weight: 7
+    },
+    {
+        id: 4,
+        productPreview: img1,
+        productTitle: "Устрицы по рокфеллеровски",
+        description: "Значимость этих проблем настолько очевидна, что укрепление и развитие структуры",
+        price: 2700,
+        weight: 500
+    },
+    {
+        id: 5,
+        productPreview: img1,
+        productTitle: "Устрицы по рокфеллеровски",
+        description: "Значимость этих проблем настолько очевидна, что укрепление и развитие структуры",
+        price: 2700,
+        weight: 500
+    },
+    {
+        id: 6,
+        productPreview: img2,
+        productTitle: "Свиные ребрышки на гриле с зеленью",
+        description: "Не следует, однако забывать, что реализация намеченных плановых",
+        price: 1600,
+        weight: 750
+    },
+    {
+        id: 7,
+        productPreview: img2,
+        productTitle: "Свиные ребрышки на гриле с зеленью",
+        description: "Не следует, однако забывать, что реализация намеченных плановых",
+        price: 1600,
+        weight: 750
+    },
+    {
+        id: 8,
+        productPreview: img3,
+        productTitle: "Креветки по-королевски в лимонном соке",
+        description: "Значимость этих проблем настолько очевидна, что укрепление и развитие структуры обеспечивает широкому кругу",
+        price: 1820,
+        measure: "шт.",
+        weight: 7
+    },
+    {
+        id: 9,
+        productPreview: img1,
+        productTitle: "Устрицы по рокфеллеровски",
+        description: "Значимость этих проблем настолько очевидна, что укрепление и развитие структуры",
+        price: 2700,
+        weight: 500
+    },
+
+];
+
+export default products;
