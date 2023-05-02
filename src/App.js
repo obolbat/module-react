40ac631636c6cbc64b5facef8c9daaecb8dbf623
obolbat <it-for-bi@yandex.ru>
2023-05-02 17:57:53 +0600

    Реализована страницы с корзиной товаров

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import './Products/reset.css'
-import Products from './Products';
-import PageHeader from './PageHeader';
-import './App.css';
 import React from "react";
+
+import Products from './components/ProductPage';
+import PageHeader from './pages/ProductHeader';
+import Basket from './Basket';
+
+import './App.css';
+import './components/ProductPage/reset.css'
 
 function App() {
     return (
         <>
-            <PageHeader>
-            </PageHeader>
-            <Products/>
+            <PageHeader/>
+            {/*<Products/>*/}
+            <Basket/>
         </>
 );
 }
---
+++ src/bd/basketItems.js
@@ -0,0 +1,26 @@
+import img1 from "../assets/1.png";
+import img2 from "../assets/2.png";
+import img3 from "../assets/3.png";
+
+const basketItems = [
+    {
+        id: 1,
+        productPreview: img1,
+        productTitle: "Устрицы по рокфеллеровски",
+        price: 2700
+    },
+    {
+        id: 2,
+        productPreview: img2,
+        productTitle: "Свиные ребрышки на гриле с зеленью",
+        price: 1600
+    },
+    {
+        id: 3,
+        productPreview: img3,
+        productTitle: "Креветки по-королевски в лимонном соке",
+        price: 1820
+    }
+];
+
+export default basketItems;
